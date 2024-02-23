907ca14991b7935f763614c39456e38e8b97ccfb
mrpinkcat <gatien.anizan@gmail.com>
2024-02-23 11:33:44 +0100

    fix(socket): change socket url for matching the prod

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -9,7 +9,7 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : import.meta.env.VITE_SOCKET;
+const URL = process.env.NODE_ENV === 'production' ? undefined : import.meta.env.VITE_API;
 
 export const socket = io(URL);
 
