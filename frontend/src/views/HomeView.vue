3ecbb877c0fdc7ce906c758e74c83f69fa2edf7e
Samuel GUENIER <guenier.samuel@gmail.com>
2024-02-25 23:07:46 +0100

    fix(Room): cannont join nor create if already in a room

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -25,14 +25,20 @@
       Admin
     </RouterLink>
     <RouterLink
-      v-if="isLogged && !isQuizzesLoading && quizzes.length > 0"
+      v-if="isLogged && !isQuizzesLoading && quizzes.length > 0 && !profile.currentRoom"
       to="/room"
     >
       Create a room
     </RouterLink>
     <JoinRoom
-      v-if="isLogged"
+      v-if="isLogged && !profile.currentRoom"
     />
+    <button
+      v-if="isLogged && profile.currentRoom"
+      @click="leaveRoom()"
+    >
+      Leave room {{ profile.currentRoom.id }}
+    </button>
     <span v-if="!isQuizzesLoading && quizzes.length === 0">
       ⚠️⚠️⚠️ No quizzes found, ask an admin to add some before creating a room ⚠️⚠️⚠️
     </span>
@@ -46,14 +52,21 @@
 <script setup>
 import { useAuthStore } from '@/stores/authStore';
 import { useQuizzStore } from '@/stores/quizzStore';
+import { useRoomStore } from '@/stores/roomStore';
 import { storeToRefs } from 'pinia';
 import JoinRoom from '@/components/JoinRoom.vue';
 
 const authStore = useAuthStore();
 const quizzStore = useQuizzStore();
+const roomStore = useRoomStore();
 
 quizzStore.getAllQuizzes();
 
 const { isLogged, isAdmin, profile } = storeToRefs(authStore);
 const { quizzes, isQuizzesLoading } = storeToRefs(quizzStore);
+
+const leaveRoom = async () => {
+  await roomStore.leaveRoom();
+  await authStore.getProfile();
+};
 </script>
